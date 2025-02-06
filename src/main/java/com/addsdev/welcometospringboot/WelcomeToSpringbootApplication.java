4fedf5be21cd9da01285facefa2bda5744a0bfa6
Adrian Ruiz <addsdeveloper@gmail.com>
2025-02-05 23:07:17 -0500

    Initial config

---
+++ src/main/java/com/addsdev/welcometospringboot/WelcomeToSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.addsdev.welcometospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeToSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeToSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addsdev/welcometospringboot/WelcomeToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addsdev.welcometospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeToSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
